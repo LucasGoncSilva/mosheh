f8d04302d73bf29d5725a3a5c8996fd5d9f4c4f5
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-11-14 12:08:29 -0300

    adds docstrings for classes and funcs in test dir

--- tests/dir/another.py
+++ tests/dir/another.py
@@ -2,8 +2,19 @@ from tests.example import ExampleTest, create_example_class
 
 
 class AnotherExampleTest(ExampleTest):
-    def def_constant(self) -> str:
+    """
+    Simple description.
+
+    A way more longer and detailed text with no util stuff for
+    pratical reading but extremely relevance for doc testing and
+    spending time for those who really cares about reading it.
+    """
+
+    def get_constant(self) -> str:
         return self.SOME_CONST
 
     def custom_create(self) -> None:
         _cls: ExampleTest = create_example_class()
+        random_str: str = f'{self.get_constant()}{_cls}'
+
+        del random_str
