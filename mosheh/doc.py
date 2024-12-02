bd88c9feae86ba049d9fbd77648d44fb2f723c3e
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-12-02 12:18:22 -0300

    inserts docstrings for doc.py fns

--- mosheh/doc.py
+++ mosheh/doc.py
@@ -39,7 +39,7 @@ def generate_doc(
     - MkDocs: A static site generator that's geared towards project documentation.
     - Codebase Processing: The function relies on `process_codebase` to handle the
       codebase structure and populate the documentation content based on Python files
-      and their statements.
+      and their stmts.
     - Configuration: Builds a `mkdocs.yml` configuration file with project details,
       including repository information and editing URI.
 
@@ -109,19 +109,19 @@ def codebase_to_markdown(filedata: list[StandardReturn], basedir: str) -> str:
     """
     Converts a file's processed data into a structured Markdown representation.
 
-    This function processes a list of statements extracted from a Python file and
-    generates a Markdown-formatted string. It categorizes statements into imports,
+    This function processes a list of stmts extracted from a Python file and
+    generates a Markdown-formatted string. It categorizes stmts into imports,
     constants, classes, functions, and assertions, ensuring that each type is
-    documented appropriately. If a category has no statements, a default informational
+    documented appropriately. If a category has no stmts, a default informational
     message is added.
 
     Key concepts:
-    - Statement Handling: The function processes different types of statements
+    - Statement Handling: The function processes different types of stmts
       (imports, assignments, class and function definitions, etc.) and organizes
       them into corresponding sections.
     - Markdown Generation: The output is formatted using a predefined Markdown
       template (`FILE_MARKDOWN`) that structures the documentation by category.
-    - Category Defaults: If no statements exist for a particular category, an
+    - Category Defaults: If no stmts exist for a particular category, an
       informational block is added to indicate its absence.
 
     Example:
@@ -564,40 +564,33 @@ def process_codebase(
     basedir: str = '',
 ) -> None:
     """
-    Recursively processes a CodebaseDict generating `.mf` files for each module
+    Recursively processes a codebase and generates documentation for each file.
 
-    This function traverses the codebase, converting the content of each Python file
-    (represented as a list of statements) into a Markdown file. It maintains the folder
-    structure, creating necessary directories and writing the corresponding `.md` files
-    to the specified output path.
+    This function traverses a codebase structure, processes each file's statements,
+    and generates corresponding Markdown documentation. The documentation is written
+    to the specified output directory. If the codebase contains nested dictionaries,
+    the function recursively processes each nested level.
+
+    Key concepts:
+    - Recursive Processing: Handles both individual files and nested dirs.
+    - File Documentation: Converts statements into documentation and writes to output.
+    - Directory Structure: Preserves directory structure in the output documentation.
 
     Example:
     ```python
-    codebase: dict[str, CodebaseDict] | dict[str, list[StandardReturn]] = {
-        'module1.py': [{'statement': Statement.ClassDef, 'name': 'MyClass', ...}],
-        'subdir': {
-            'module2.py': [{
-                'statement': Statement.FunctionDef, 'name': 'my_function', ...
-            }]
-        }
-    }
-    process_codebase(codebase, '/path/to/source', '/path/to/docs')
-    # Generates docs in '/path/to/docs/docs/module1.md' and 'subdir/module2.md'
+    process_codebase(codebase, '/root', '/output')
+    # Processes the codebase and generates documentation in the '/output' directory.
     ```
 
-    :param codebase: a dictionary representing the codebase structure, where keys are
-                    file or dir names, and values are either subdirectories or lists
-                    of parsed statements
+    :param codebase: The codebase to process, which can contain files or nested dirs.
     :type codebase: dict[str, CodebaseDict] | dict[str, list[StandardReturn]]
-    :param root: the root path of the source code being processed
+    :param root: The root directory of the project.
     :type root: str
-    :param exit: the output dir where the Markdown documentation will be generated
+    :param exit: The output directory where documentation will be saved.
     :type exit: str
-    :param basedir: the base dir for the current level of the codebase, used
-                    for maintaining the folder structure in the output
-    :type basedir: str, optional
-    :return: nothing, just generates `.md` files in the specified output path
-    :rtype: None
+    :param basedir: The base directory used during the recursive traversal.
+    :type basedir: str, optional, default is ''
+    :return: None
     """
 
     parents: list[str] = list(codebase.keys())
@@ -608,42 +601,145 @@ def process_codebase(
         new_path: str = path.join(basedir, key)
 
         if isinstance(value, list):
-            if len(value):
-                content: str = codebase_to_markdown(value, new_path)
-
-                output_file_path: str = path.join(
-                    docs_path, new_path.removeprefix(root) + '.md'
-                )
-                folder_path = output_file_path.split(key)[0]
-
-                if not path.exists(folder_path):
-                    mkdir(folder_path)
-                with open(output_file_path, 'w', encoding='utf-8') as f:
-                    f.write(content)
-
-                nav_path: list[str] = [
-                    i for i in folder_path.removeprefix(docs_path).split(path.sep) if i
-                ]
-
-                if not nav_path:
-                    nav_path.append('Root')
-
-                for i in range(len(nav_path)):
-                    sub_nav_path: str = f'{path.sep}'.join(nav_path[0 : i + 1])
-
-                    if sub_nav_path not in NAV_DIRS:
-                        NAV_DIRS.append(sub_nav_path)
-                        md_line: str = indent_code(
-                            f'- {sub_nav_path.split(path.sep)[-1]}:', 2 * (i + 1)
-                        )
-                        NAV_MD.append(f'{md_line}\n')
-
-                    if i + 1 == len(nav_path):
-                        md_line: str = indent_code(
-                            f'- {key}: {output_file_path.removeprefix(f"{docs_path}{path.sep}")}',
-                            2 * (i + 2),
-                        )
-                        NAV_MD.append(f'{md_line}\n')
-
+            __process_file(key, value, new_path, root, docs_path)
         else:
             process_codebase(value, root, exit, new_path)
+
+
+def __process_file(
+    key: str,
+    stmts: list[StandardReturn],
+    file_path: str,
+    root: str,
+    docs_path: str,
+) -> None:
+    """
+    Processes a file's stmts and generates corresponding documentation.
+
+    This function converts a list of stmts into a Markdown document, writes
+    the content to the appropriate file path, and updates the navigation structure
+    for the documentation. If the necessary folder path does not exist, it is created.
+
+    Key concepts:
+    - Statement Processing: Converts stmts into Markdown format.
+    - File Writing: Saves the generated content to the appropriate file.
+    - Navigation Update: Updates the documentation's navigation structure.
+
+    Example:
+    ```python
+    __process_file('module_name', stmts, 'src/module.py', '/root', '/docs')
+    # Processes the stmts from 'module.py' and generates corresponding markdown docs.
+    ```
+
+    :param key: The key representing the module or file being processed.
+    :type key: str
+    :param stmts: The list of stmts that represent the code to be documented.
+    :type stmts: list[StandardReturn]
+    :param file_path: The path to the source file, used to derive output locations.
+    :type file_path: str
+    :param root: The root directory of the project.
+    :type root: str
+    :param docs_path: The path to the documentation directory.
+    :type docs_path: str
+    :return: None
+    """
+
+    if not stmts:
+        return
+
+    content: str = codebase_to_markdown(stmts, file_path)
+    output_file_path: str = path.join(docs_path, file_path.removeprefix(root) + '.md')
+    folder_path: str = path.dirname(output_file_path)
+
+    if not path.exists(folder_path):
+        mkdir(folder_path)
+
+    __write_to_file(output_file_path, content)
+    __update_navigation(folder_path, docs_path, key, output_file_path)
+
+
+def __write_to_file(file_path: str, content: str) -> None:
+    """
+    Writes content to a specified file.
+
+    This function opens a file at the given path in write mode and writes the provided
+    content to it. The content is written using UTF-8 encoding, ensuring compatibility
+    with various char sets.
+
+    Key concepts:
+    - File Writing: Opens a file for writing and writes the content.
+    - UTF-8 Encoding: Ensures the file is written with UTF-8 for proper char handling.
+
+    Example:
+    ```python
+    __write_to_file('output.md', 'This is some content.')
+    # Writes the content "This is some content." to 'output.md'.
+    ```
+
+    :param file_path: The path to the file where the content will be written.
+    :type file_path: str
+    :param content: The content to be written to the file.
+    :type content: str
+    :return: None
+    """
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+def __update_navigation(
+    folder_path: str, docs_path: str, key: str, output_file_path: str
+) -> None:
+    """
+    Updates the navigation structure for documentation generation.
+
+    This function builds and updates a nested navigation structure for documentation
+    files based on the provided folder path and file location. It ensures that
+    each segment of the path is represented in the navigation hierarchy, maintaining
+    the correct indentation levels.
+
+    Key concepts:
+    - Navigation Hierarchy: Constructs a structured navigation tree from folder paths.
+    - Indentation: Adjusts indentation dynamically based on folder depth.
+    - Path Normalization: Handles path manipulation to generate correct relative paths.
+
+    Example:
+    ```python
+    __update_navigation(
+        'project/docs/module',
+        'project/docs',
+        'functions',
+        'project/docs/module/functions.md',
+    )
+    # Updates the global NAV_DIRS and NAV_MD structs with the right navigation entries.
+    ```
+
+    :param folder_path: The full path to the folder containing the documentation files.
+    :type folder_path: str
+    :param docs_path: The root path to the documentation directory.
+    :type docs_path: str
+    :param key: The label or name for the current documentation entry.
+    :type key: str
+    :param output_file_path: The path to the output documentation file.
+    :type output_file_path: str
+    :return: None
+    """
+
+    nav_path: list[str] = [
+        segment
+        for segment in folder_path.removeprefix(docs_path).split(path.sep)
+        if segment
+    ]
+    nav_path = nav_path or ['Root']
+
+    for i in range(len(nav_path)):
+        sub_nav_path: str = path.sep.join(nav_path[: i + 1])
+        if sub_nav_path not in NAV_DIRS:
+            NAV_DIRS.append(sub_nav_path)
+            md_line: str = indent_code(f'- {nav_path[i]}:', 2 * (i + 1))
+            NAV_MD.append(f'{md_line}\n')
+
+        if i + 1 == len(nav_path):
+            md_file_path: str = output_file_path.removeprefix(docs_path + path.sep)
+            md_line: str = indent_code(f'- {key}: {md_file_path}', 2 * (i + 2))
+            NAV_MD.append(f'{md_line}\n')
