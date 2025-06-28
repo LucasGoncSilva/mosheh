25581f405bff0b010a50e0cb6cda896f038d1731
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2025-06-27 21:49:39 -0300

    docs: Update Mosheh's package docstrings

--- mosheh/metadata.py
+++ mosheh/metadata.py
@@ -5,13 +5,16 @@ Here are just info about Mosheh itself, data about data. No more than this.
 """
 
 __author__ = 'LucasGonc'
-__copyright__ = 'Copyright (c) 2024 Lucas Gonçalves da Silva'
-__credits__ = ['LucasGonc']
-__license__ = 'MIT'
 __maintainer__ = 'LucasGonc'
+__credits__ = ['LucasGonc']
 __email__ = 'lucasgoncsilva04@gmail.com'
+
+__license__ = 'MIT'
 __repository__ = 'https://github.com/LucasGoncSilva/mosheh'
 __keywords__ = ['CLI', 'Python', 'documentation', 'MkDocs', 'automation', 'generation']
+
 __version__ = '1.3.4'
 __date__ = '2025-01-07'
 __status__ = 'Production'
+
+__copyright__ = 'Copyright (c) 2025 Lucas Gonçalves da Silva'
