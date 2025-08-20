767cfefd6797d187a83cd30d47d429f0f3ea8129
Lucas Gonçalves da Silva <90107526+LucasGoncSilva@users.noreply.github.com>
2025-08-19 23:18:15 -0300

    feat: Set benchmark with Memray and Scalene (#28)
    * perf: Set benchmark scripting
    
    * docs: Update with benchmarking
    
    * config: Remove uv.lock
    
    * fix: Move Memray to bench group
    
    * test: Update PROJECT to real case

--- tests/PROJECT/CORE/urls.py
+++ tests/PROJECT/CORE/urls.py
@@ -1,22 +1,59 @@
-"""
-URL configuration for CORE project.
+from os import getenv
+from typing import Final
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.views import (
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
+from django.urls import URLPattern, URLResolver, include, path
 
 
-urlpatterns = [path('admin/', admin.site.urls), path('/', include('dummy.urls'))]
+urlpatterns: list[URLResolver | URLPattern] = [
+    # System functionality's pages
+    path(
+        'reset',
+        PasswordResetView.as_view(template_name='account/password_reset.html'),
+        name='password_reset',
+    ),
+    path(
+        'reset-sent',
+        PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>',
+        PasswordResetConfirmView.as_view(
+            template_name='account/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset-complete',
+        PasswordResetCompleteView.as_view(
+            template_name='account/password_reset_complete.html'
+        ),
+        name='password_reset_complete',
+    ),
+    path('captcha/', include('captcha.urls')),
+    path('send-email/', include('mail.urls')),
+    path('error/', include('err.urls')),
+    # User's pages
+    path('', include('home.urls')),
+    path('account/', include('account.urls')),
+    path('secrets/', include('secret.urls')),
+    path('general/', include('general.urls')),
+    path('plans/', include('plans.urls')),
+]
+
+if getenv('DJANGO_SETTINGS_MODULE', 'CORE.settings.dev') == 'CORE.settings.dev':
+    urlpatterns += [
+        path(f'{getenv("ADMIN", "__manager__")}/', admin.site.urls)
+    ] + debug_toolbar_urls()
+
+handler403: Final[str] = 'err.views.handle403'
+handler404: Final[str] = 'err.views.handle404'
+handler500: Final[str] = 'err.views.handle500'
